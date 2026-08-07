0d97f742018416264b3f7068c84640b2f706ae5f
apocalypse9949 <125962989+apocalypse9949@users.noreply.github.com>
2026-08-07 01:25:15 +0000

    Bolt: optimize compiler hot paths
    - Replaced standard library character classification in lexer with a static lookup array.
    - Replaced sequential strncmp checks for keyword tokenization with a length-based switch statement.
    - Added first-character fast-path checks before calling strcmp in tight symbol lookup and registry lookup loops.

--- src/parser/utils/utils_registry.c
+++ src/parser/utils/utils_registry.c
@@ -451,6 +451,7 @@ ASTNode *find_struct_def(ParserContext *ctx, const char *name)
         while (s)
         {
             if ((s->type == NODE_STRUCT || s->type == NODE_ENUM) &&
+                (s->type == NODE_STRUCT ? s->strct.name[0] : s->enm.name[0]) == name[0] &&
                 strcmp((s->type == NODE_STRUCT ? s->strct.name : s->enm.name), name) == 0)
             {
                 if (!(s->type == NODE_STRUCT && s->strct.is_incomplete))
@@ -465,7 +466,7 @@ ASTNode *find_struct_def(ParserContext *ctx, const char *name)
     Instantiation *i = ctx->instantiations;
     while (i)
     {
-        if (strcmp(i->name, name) == 0)
+        if (i->name[0] == name[0] && strcmp(i->name, name) == 0)
         {
             CACHE_RESULT(i->struct_node);
         }
@@ -476,6 +477,7 @@ ASTNode *find_struct_def(ParserContext *ctx, const char *name)
     while (s)
     {
         if ((s->type == NODE_STRUCT || s->type == NODE_ENUM) &&
+            (s->type == NODE_STRUCT ? s->strct.name[0] : s->enm.name[0]) == name[0] &&
             strcmp((s->type == NODE_STRUCT ? s->strct.name : s->enm.name), name) == 0)
         {
             CACHE_RESULT(s);
@@ -486,11 +488,13 @@ ASTNode *find_struct_def(ParserContext *ctx, const char *name)
     StructRef *r = ctx->parsed_structs_list;
     while (r)
     {
-        if (r->node->type == NODE_STRUCT && strcmp(r->node->strct.name, name) == 0)
+        if (r->node->type == NODE_STRUCT && r->node->strct.name[0] == name[0] &&
+            strcmp(r->node->strct.name, name) == 0)
         {
             CACHE_RESULT(r->node);
         }
-        if (r->node->type == NODE_ENUM && strcmp(r->node->enm.name, name) == 0)
+        if (r->node->type == NODE_ENUM && r->node->enm.name[0] == name[0] &&
+            strcmp(r->node->enm.name, name) == 0)
         {
             CACHE_RESULT(r->node);
         }
@@ -500,8 +504,8 @@ ASTNode *find_struct_def(ParserContext *ctx, const char *name)
     ZenSymbol *all = ctx->all_symbols;
     while (all)
     {
-        if ((all->kind == SYM_STRUCT || all->kind == SYM_ENUM) && strcmp(all->name, name) == 0 &&
-            all->data.node)
+        if ((all->kind == SYM_STRUCT || all->kind == SYM_ENUM) && all->name[0] == name[0] &&
+            strcmp(all->name, name) == 0 && all->data.node)
         {
             CACHE_RESULT(all->data.node);
         }
@@ -511,7 +515,7 @@ ASTNode *find_struct_def(ParserContext *ctx, const char *name)
     StructDef *d = ctx->struct_defs;
     while (d)
     {
-        if (strcmp(d->name, name) == 0)
+        if (d->name[0] == name[0] && strcmp(d->name, name) == 0)
         {
             CACHE_RESULT(d->node);
         }
@@ -521,7 +525,8 @@ ASTNode *find_struct_def(ParserContext *ctx, const char *name)
     StructRef *e = ctx->parsed_enums_list;
     while (e)
     {
-        if (e->node->type == NODE_ENUM && strcmp(e->node->enm.name, name) == 0)
+        if (e->node->type == NODE_ENUM && e->node->enm.name[0] == name[0] &&
+            strcmp(e->node->enm.name, name) == 0)
         {
             CACHE_RESULT(e->node);
         }
